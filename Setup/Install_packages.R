ad9688517296bb539d7cd04597e334326dd93463
Krabberod <anders.krabberod@gmail.com>
2023-04-11 13:23:54 +0200

    added content

---
+++ Setup/Install_packages.R
@@ -0,0 +1,70 @@
+# BIO9905MERG1_V23
+# Please make sure that you have the required R packages installed.
+# You can get a list of the package already installed on your computer s by executing
+
+installed.packages()
+
+# in R alternatively you can just run the installation for each package
+# to make sure that you have the latest version.
+# Run these installation commands line-by-line in R (or Rstudio)
+# and answer yes if you are asked to update any previously installed packages:
+
+
+install.packages("readxl")    # To read excel files
+install.packages("metacoder") 
+install.packages("tidyverse") # To manipulate and visualize data
+# Tidyverse is a collection of packages that are really useful for data manipulation,
+# an visualization. These packages are included and you do not need to install them 
+# separately (although you can if you want to)
+# install.packages("dplyr")     # To manipulate dataframes
+# install.packages("tibble")    # To work with data frames
+# install.packages("tidyr")     # To work with data frames
+# install.packages("stringr")   # To manipulate strings
+# install.packages("ggplot2")   # To do plots
+# install.packages("readr")     # To read and write files
+# install.packages("kableExtra")
+
+install.packages("magrittr")
+
+# Some packages, (for instance dada2 and phyloseq) are in a different repository
+# called Bioconductor. To install package from the Bioconductor repository
+# you need to use the BiocManager package: 
+if (!requireNamespace("BiocManager", quietly = TRUE))  install.packages("BiocManager")
+BiocManager::install(c("dada2", "phyloseq","Biostrings","PCAtools"))
+
+# If you run into problems when running the above line try to install one package
+# at the time. i.e.: 
+# if (!requireNamespace("BiocManager", quietly = TRUE))  install.packages("BiocManager")
+# BiocManager::install("dada2")
+
+# install.packages("Matrix")	# Manipulation of large matrices
+# install.packages("wTO") 	# Newtork analysis
+# install.packages("igraph")	# Network analysis
+# install.packages("standardize")
+# install.packages("vegan")
+
+# Packages related to ecological analysis. Only shown as examples.
+# install.packages("spaa")           # Installs the ecological package spaa
+# install.packages("compositions")   # To work with compositional data
+# install.packages("zCompositions")  # To work with compositional data
+# install.packages("devtools")       # Developer tools
+# install.packages("mixOmics")       # Multivariate methods
+# install.packages("ape")            # Phylogenetic tools
+# install.packages("recluster")      # Clustering tools
+# install.packages("dendextend")     # To work with dendrograms
+# install.packages("corrplot")       # makes nice correlation plots
+# install.packages("RcmdrMisc")      # diverse tools
+
+# BiocManager::install("microbiome")
+# BiocManager::install("SpiecEasi") #Network construction
+
+# Network packages
+# library(devtools)
+# install_github("zdk123/SpiecEasi")
+# devtools::install_github("pr2database/pr2database") # Installs directly from github resources that are not in R repos
+# devtools::install_github("GuillemSalazar/EcolUtils") # Installs other tools for ecological analyses
+# devtools::install_github('fawda123/ggord')
+# This package might cause a problem. Here are some possible solutions:
+# https://thecoatlessprofessor.com/programming/cpp/r-compiler-tools-for-rcpp-on-macos/
+# https://stackoverflow.com/questions/37776377/error-when-installing-an-r-package-from-github-could-not-find-build-tools-neces
+
---
+++ Setup/Readme.md
@@ -0,0 +1,19 @@
+### Under construction
+
+We will use R (version 4.0.5 or later) and Rstudio (version 1.4.1 or later) in this course. 
+
+Everybody should download and install R (https://www.r-project.org/), Rstudio (https://www.rstudio.com/) and the [required packages](Install_required_packages.R) before the course starts.
+
+It is preferable that you have all packages installed before the course starts. For those of you having trouble with installing the packages, we will provide some help Monday afternoon, 4pm, but we will not be able to provide comprehensive IT-service though.
+
+These are the required packages for the hands-on exercises of the course: 
+```
+install.packages("readxl")    # To read excel files
+install.packages("tidyverse") # To manipulate and visualize data
+install.packages("metacoder")
+install.packages("magrittr") 
+
+if (!requireNamespace("BiocManager", quietly = TRUE))  install.packages("BiocManager")
+BiocManager::install(c("dada2", "phyloseq","Biostrings","PCAtools"))
+```
+More packages, including those that have been mentione, but not necessarily demonstrated can be found in the script [R packages](Install_packages.R).
